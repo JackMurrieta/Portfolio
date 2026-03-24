9023bd56e966a6a545c7979eb437e419443cfd83
JackMurrieta <jack.murrieta252851@potros.itson.edu.mx>
2026-03-24 16:54:04 -0700

    feat: import app.css a app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Header from "./components/header/header";
 import JackMain from "./pages/main/main";
 import ProjectsSection from "./pages/projects/projects";
 import TechStack from "./pages/techStack/techStack";
+import "./App.css";
 
 function App() {
   return (
